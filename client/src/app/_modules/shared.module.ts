d6ec60d97d616996db004b11585d11da1694f1a8
A-rielox <arielox.ag@gmail.com>
2023-08-20 23:31:50 +0200

    listo seccion 10

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -5,6 +5,7 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
    declarations: [],
@@ -16,7 +17,10 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
       ToastrModule.forRoot({
          positionClass: 'toast-bottom-right',
       }),
+      NgxSpinnerModule.forRoot({
+         type: 'pacman',
+      }),
    ],
-   exports: [BsDropdownModule, ToastrModule, TabsModule],
+   exports: [BsDropdownModule, ToastrModule, TabsModule, NgxSpinnerModule],
 })
 export class SharedModule {}
